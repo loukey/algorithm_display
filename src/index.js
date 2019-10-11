528e15eb2554c96093bbd1315690cb5a1248cc08
ycl <529768926@qq.com>
2019-10-11 13:13:58 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by za on 2019/10/2.
+ */
+import ReactDOM from 'react-dom'
+
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {liked: false}
+    }
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return e(
+            'button',
+            {onClick: () => this.setState({liked: true})},
+            'Like'
+        );
+    }
+}
+
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer)
